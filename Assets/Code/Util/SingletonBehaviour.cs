2e1539f7289660c491dea576d16ed71a370625fa
Mike <weldonconsulting@gmail.com>
2018-04-22 20:29:50 -0700

    first commit

---
+++ Assets/Code/Util/SingletonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public abstract class SingletonBehaviour : MonoBehaviour {
+
+    private static SingletonBehaviour _instance;
+    public static SingletonBehaviour Instance {
+        get { return _instance; }
+        protected set {
+            if (_instance != null) throw new Exception($"Only one {value.GetType().Name} expected.");
+            _instance = value;
+        }
+    }
+
+
+    protected virtual void Awake() {
+        SingletonBehaviour.Instance = this;
+    }
+
+    protected virtual void OnDestroy() {
+        if (SingletonBehaviour.Instance == this)
+            SingletonBehaviour.Instance = null;
+    }
+
+}
+
